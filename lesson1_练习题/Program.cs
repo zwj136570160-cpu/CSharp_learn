92276d545bed229ed5e886306326bee04348232b
ZWJ136570160-CPU <zwj136570160@gmail.com>
2025-12-22 09:00:19 +0800

    测试GitHub

---
+++ lesson1_练习题/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace lesson1_练习题
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lesson1_练习题讲解");
+            
+            //请用户输入用户名、年龄、班级
+            Console.WriteLine("请输入用户名、年龄、班级");
+            Console.ReadLine()
+        }
+    }
+}
